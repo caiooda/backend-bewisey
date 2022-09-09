9753ff786804f7fd6d8ac82fc8e641eb504cfc1d
Caio Oda <devcaiooda@hotmail.com>
2022-09-08 21:29:45 -0300

    #v1.0.1

--- src/middlewares/Routes.ts
+++ src/middlewares/Routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import AulasRoutes from "../infra/routes/AulaRoutes";
 import AutenticacaoRoutes from "../infra/routes/AutenticacaoRoutes";
 import UsuarioRoutes from "../infra/routes/UsuarioRoutes";
 
@@ -7,5 +8,6 @@ const routes = Router();
 
 routes.use("/api/usuario", UsuarioRoutes);
 routes.use("/api/auth", AutenticacaoRoutes);
+routes.use("/api/aula", AulasRoutes);
 
 export default routes;
